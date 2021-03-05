dfb34a5b99786bc4f500194968d150785edecee5
Brangy Castro <brangy.castro1@gmail.com>
2021-03-04 22:14:49 -0500

    ready

--- index.py
+++ index.py
@@ -20,23 +20,23 @@ app.register_blueprint(metreg_api)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', page="index")
 
 @app.route('/index2')
 def index2():
-    return render_template('index2.html')
+    return render_template('index2.html', page="index2")
     
 @app.route('/index3')
 def index3():
-    return render_template('index3.html')
+    return render_template('index3.html', page="index3")
     
 @app.route('/index4')
 def index4():
-    return render_template('index4.html')
+    return render_template('index4.html', page="index4")
     
 @app.route('/index5')
 def index5():
-    return render_template('index5.html')
+    return render_template('index5.html', page="index5")
 
 
 if __name__ == '__main__':
